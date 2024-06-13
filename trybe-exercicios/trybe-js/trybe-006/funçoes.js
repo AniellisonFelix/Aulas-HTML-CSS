7459ff48d46ebabab855a20d3be89688f39c12c9
Aniellison Felix <aniellisom@gmail.com>
2024-06-13 13:08:43 -0300

    atualizando html

--- trybe-exercicios/trybe-js/trybe-006/funçoes.js
+++
@@ -1,9 +0,0 @@
-const trybeBankCustomers = ['oliva', 'mati', 'gus'];
-const pirilamposCustomers = ['noel', 'carol', 'angelo', 'kissyla'];
-
-function findElement(array, element) {
-    return array.includes(element);
-};
-
-console.log(findElement(trybeBankCustomers, 'guss'));
-console.log(findElement(pirilamposCustomers, 'carol'));
--- trybe-exercicios/trybe-js/trybe-007/arrowfunction02.js
+++
@@ -1,5 +0,0 @@
-const countWords = (phrase) => {
-    return phrase.split(' ').length;
-};
-
-console.log(countWords('fala doido, blz?'));
